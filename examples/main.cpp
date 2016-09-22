ec11ddef6e0706c41b12ed3d85370baa783cd47e
Oly <hoit@bk.ru>
2016-09-23 00:25:49 +0300

    Create main.cpp

---
+++ examples/main.cpp
@@ -0,0 +1,14 @@
+#include "binary_search_tree.h"
+int main()
+{
+	BinarySearchTree<int> tree= { 8,10,3,6,7,14,13 }; 
+	tree.find(10);
+	tree.insert(5);
+	std::cout << tree;
+	std::ofstream file("C:\\tree.txt");
+	file << tree;
+	BinarySearchTree<int> tree2;
+	std::cin >> tree2;
+	auto a = tree.find(8);
+	std::cout << (*a) << std:: endl;
+}

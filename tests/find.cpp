9a5c8d411837e075f5c08bde70362cc746034d51
Oly <hoit@bk.ru>
2016-09-30 18:39:10 +0400

    Update find.cpp

--- tests/find.cpp
+++ tests/find.cpp
@@ -1,1 +1,59 @@
+#include "catch.hpp"
+#include "binary_search_tree.h"
+
+SCENARIO ("if object exist")
+{
+   GIVEN("object")
+   {
+       int object=8;
+       BinarySearchTree <int> tree;
+       WHEN("find")
+       {
+           THEN(":)")
+           {
+               REQUIRE(tree.find(object)!=nullptr);
+           }
+       }
+   }
+}
+
+SCENARIO("if object doesn't exist")
+{
+    GIVEN("object") {
+        int object = 150;
+        BinarySearchTree<int> tree{1, 2, 3, 4, 5, 6};
+        WHEN("not find") {
+            tree.find(object);
+            THEN(":(") {
+                REQUIRE(tree.find(object) == nullptr);
+            }
+        }
+    }
+}
+SCENARIO("for constant and non-constant")
+{
+    GIVEN("2 objects:const and non-const")
+    {
+      BinarySearchTree <int> tree1 {1, 2, 3, 4, 5};
+      const BinarySearchTree <int> tree2 {1, 2, 7, 9, 10};
+      int object=12;
+        WHEN("find")
+        {
+            tree1.find(object);
+            tree2.find(object);
+            THEN("ok")
+            {
+                REQUIRE(tree1.find(object)!=nullptr);
+                REQUIRE(tree2.find(object)!=nullptr);
+            }
+            THEN("not ok")
+            {
+                REQUIRE(tree1.find(object)==nullptr);
+                REQUIRE(tree2.find(object)==nullptr);
+            }
+        }
+            
+        }
+    }
+
 

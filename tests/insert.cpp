74693d699e5c5e73f425e2992df386fa4bb83fc7
Oly <hoit@bk.ru>
2016-11-25 00:13:51 +0300

    Update insert.cpp

--- tests/insert.cpp
+++ tests/insert.cpp
@@ -1,82 +1,106 @@
-//
-// insert.cpp
-
-//- если элемент уже присутствует в дереве, то метод должен вернуть false
-//- если элемент не присутствует в дереве, то метод должен вернуть true
-
-//- если вставляемый элемент меньше элемента находящегося в корне дерева, то он должен быть вставлен слева от него
-//- если вставляемый элемент больше элемента находящегося в корне дерева, то он должен быть вставлен справа от него
-
-//- если элемент уже присутствует в дереве, то метод должен не изменять значения размера дерева
-//- если элемент не присутствует в дереве, то метод должен увеличить значение размера дерева на 1
-
 #include "catch.hpp"
 #include "binary_search_tree.h"
 
-SCENARIO("if already exist")
+
+SCENARIO ("Insert item when object already exists")
 {
-    GIVEN("object")
+    GIVEN ("object")
     {
-        BinarySearchTree<int> tree{1, 2, 3, 4, 5, 6, 7};
-        int object= 5;
-        WHEN("insert")
+        BinarySearchTree <int> tree={1,2};
+        int object=5;
+        WHEN ("insert")
         {
-         tree.insert(object);
-            THEN("if it already in the tree")
+            tree.insert(object);
+            THEN ("if it in the tree")
             {
                 REQUIRE(!tree.insert(object));
             }
-        }
+        };
+
     }
 }
-
-SCENARIO("if not already exist")
+SCENARIO ("Insert item when object doesn't exist")
 {
-    GIVEN("object")
+    GIVEN ("object")
     {
-        BinarySearchTree<int> tree{1, 2, 3, 4, 5, 6, 7};
-        int object= 9;
-        WHEN("insert")
+        BinarySearchTree <int> tree={1,2};
+        int object=5;
+        WHEN ("insert")
         {
-            THEN("insert ok")
-            {   
-                REQUIRE(tree.insert(object));                
+            THEN ("if it is not in the tree")
+            {
+                REQUIRE(tree.insert(object));
             }
-        }
+        };
+
     }
 }
-
 SCENARIO ("If object < root")
 {
     GIVEN ("object and root")
     {
-        BinarySearchTree <int> tree={9,4};
+        BinarySearchTree <int> tree={5,2};
         int object=1;
-        BinarySearchTree <int> tree2={9,4,1};
+        BinarySearchTree <int> rez={5,2,1};
 
         WHEN ("insert")
         {
             tree.insert(object);
-            THEN ("insert ok. (left)") 
+            THEN ("put it on the left") 
             { 
-                REQUIRE(tree==tree2);
+                REQUIRE(tree==rez);
             }
         };
     }
 }
-
 SCENARIO ("If object > root")
 {
     GIVEN ("object and root")
     {
-        BinarySearchTree <int> tree={1,3};
-        int object=5;
-        BinarySearchTree <int> tree2={1,3,5};
+        BinarySearchTree <int> tree={1,2};
+        int object=3;
+        BinarySearchTree <int> rez={1,2,3};
 
         WHEN ("insert")
         {
             tree.insert(object);
-            THEN ("insert ok. (right)") { REQUIRE(tree==tree2);}
+            THEN ("put it on the right") {REQUIRE(tree==rez);}
         };
+    }
+}
+SCENARIO ("If object already exists")
+{
+    GIVEN ("object")
+    {
+        BinarySearchTree <int> tree={1,2};
+        int object=2;
+        auto currsize=tree.size();
+        WHEN ("insert")
+        {
+            tree.insert(object);
+            THEN ("if it in the tree")
+            {
+                REQUIRE(tree.size()==currsize);
+            }
+        };
+
+    }
+}
+SCENARIO ("If object doesn't exist")
+{
+    GIVEN ("object")
+    {
+        BinarySearchTree <int> tree={1,2};
+        int object=5;
+        auto currsize=tree.size();
+        WHEN ("insert")
+        {
+            tree.insert(object);
+            THEN ("if it is not in the tree")
+            {
+                REQUIRE(tree.size()==currsize+1);
+            }
+        };
+
     }
 }

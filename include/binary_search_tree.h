7a045a0dfb5197e98c8b851df01997861e808e0a
Oly <hoit@bk.ru>
2016-11-25 00:26:48 +0300

    Update binary_search_tree.h

--- include/binary_search_tree.h
+++ include/binary_search_tree.h
@@ -12,38 +12,38 @@ class BinarySearchTree;
 template <typename T>
 std::ofstream & operator << (std::ofstream & out, const BinarySearchTree<T> & tree) {
 
-    tree.PreorderPrint(out, tree.GetRoot());
-    return out;
+	tree.PreorderPrint(out, tree.GetRoot());
+	return out;
 }
 
 template <typename T>
 std::ostream & operator << (std::ostream & out, const BinarySearchTree<T> & tree) {
 
-    tree.InorderPrint(out, tree.GetRoot());
-    return out;
+	tree.InorderPrint(out, tree.GetRoot());
+	return out;
 }
 
 template <typename T>
 std::istream & operator >> (std::istream & in, BinarySearchTree<T> & tree) {
-    T value;
-    int size;
-    std::cout << "Size: ";
-    in >> size;
+	T value;
+	int size;
+	std::cout << "Size: ";
+	in >> size;
 
-    for (int i = 0; i < size; i++)
-    {
-        in >> value;
-        tree.insert(value);
-    }
+	for (int i = 0; i < size; i++)
+	{
+		in >> value;
+		tree.insert(value);
+	}
 
-    return in;
+	return in;
 }
 
 template <typename T>
 std::ifstream & operator << (std::ifstream & out, const BinarySearchTree<T> & tree)
 {
-    tree.PreorderPrint(out, tree.GetRoot());
-    return out;
+	tree.PreorderPrint(out, tree.GetRoot());
+	return out;
 }
 
 template <typename T>
@@ -51,318 +51,319 @@ class BinarySearchTree
 {
 
 private:
-    struct Node;
-    shared_ptr <Node> root_;
-    size_t size_;
+	struct Node;
+	shared_ptr <Node> root_;
+	size_t size_;
 
-    struct Node {
-        Node(T value) : value_(value), left_(nullptr), right_(nullptr) {}
-        ~Node() {
-             left_= nullptr;
-             right_= nullptr;
+	struct Node {
+		Node(T value) : value_(value), left_(nullptr), right_(nullptr) {}
+		~Node() {
+			 left_= nullptr;
+			 right_= nullptr;
 
-        }
-        T value_;
-        shared_ptr<Node> left_;
-        shared_ptr<Node> right_;
-    };
+		}
+		T value_;
+		shared_ptr<Node> left_;
+		shared_ptr<Node> right_;
+	};
 
 public:
 
 
-    BinarySearchTree() : root_(nullptr), size_(0) {}
+	BinarySearchTree() : root_(nullptr), size_(0) {}
 
-    BinarySearchTree(const std::initializer_list<T> & list) : size_(0), root_(nullptr)
-    {
-        for (auto it = list.begin(); it != list.end(); ++it)
-        {
-            insert(*it);
-        }
-    }
-
-
-    BinarySearchTree(const BinarySearchTree& tree) : size_(tree.size_), root_(nullptr)//конструктор копирования
-    {
-        root_ = copy(tree.root_);
-    }
-
-    //BinarySearchTree(BinarySearchTree&& tree)= default {};// конструктор перемещения
+	BinarySearchTree(const std::initializer_list<T> & list) : size_(0), root_(nullptr)
+	{
+		for (auto it = list.begin(); it != list.end(); ++it)
+		{
+			insert(*it);
+		}
+	}
 
 
-    BinarySearchTree(BinarySearchTree&& tree) : size_(tree.size_), root_(std::move(tree.root_))// конструктор перемещения
-    {
+	BinarySearchTree(const BinarySearchTree& tree) : size_(tree.size_), root_(nullptr)//конструктор копирования
+	{
+		root_ = copy(tree.root_);
+	}
 
-    }
-
-
-
-    auto GetRoot() const -> shared_ptr<Node>
-    {
-        return root_;
-    }
+	//BinarySearchTree(BinarySearchTree&& tree)= default {};// конструктор перемещения
 
 
-    //void PreorderPrint(std::ostream & str, Node* thisNode) const noexcept // прямой
-    void PreorderPrint(std::ostream & str, shared_ptr<Node> thisNode) const noexcept // прямой
-    {
-        if (!thisNode)
-        {
+	BinarySearchTree(BinarySearchTree&& tree) : size_(tree.size_), root_(std::move(tree.root_))// конструктор перемещения
+	{
 
-            return;
-        }
-        str << thisNode->value_ << " ";
-        PreorderPrint(str, thisNode->left_);
-        PreorderPrint(str, thisNode->right_);
-    }
-
-    //void InorderPrint(std::ostream & str, Node* thisNode) const noexcept // симметричный
-    void InorderPrint(std::ostream & str, shared_ptr<Node> thisNode) const noexcept // симметричный
-    {
-        if (!thisNode)
-        {
-
-            return;
-        }
-
-        InorderPrint(str, thisNode->left_);
-        str << thisNode->value_ << " ";
-        InorderPrint(str, thisNode->right_);
-    }
-
-    auto size() const noexcept->size_t
-    {
-
-        return size_;
-    }
+	}
 
 
 
-    auto insert(const T & value) noexcept -> bool
-    {
-
-        shared_ptr<Node> thisNode = root_;
-        shared_ptr<Node> myNode = nullptr;
-
-        if (root_ == nullptr)
-        {
-            root_ = make_shared <Node>(value);
-            size_++;
-            return true;
-        }
-        while (thisNode)
-        {
-            myNode = thisNode;
-            if (value == myNode->value_)
-            {
-                return false;
-            }
-            else if (value < myNode->value_)
-            {
-                thisNode = myNode->left_;
-            }
-            else if (value > myNode->value_)
-            {
-                thisNode = myNode->right_;
-            }
-        }
-        if (value < myNode->value_)
-        {
-            myNode->left_ = make_shared <Node>(value);
-        }
-        else
-        {
-            myNode->right_ = make_shared <Node>(value);
-        };
-        size_++;
-        return true;
-    }
+	auto GetRoot() const -> shared_ptr<Node>
+	{
+		return root_;
+	}
 
 
-    auto find(const T & value) const noexcept -> const T*
-    {
+	//void PreorderPrint(std::ostream & str, Node* thisNode) const noexcept // прямой
+	void PreorderPrint(std::ostream & str, shared_ptr<Node> thisNode) const noexcept // прямой
+	{
+		if (!thisNode)
+		{
+
+			return;
+		}
+		str << thisNode->value_ << " ";
+		PreorderPrint(str, thisNode->left_);
+		PreorderPrint(str, thisNode->right_);
+	}
+
+	//void InorderPrint(std::ostream & str, Node* thisNode) const noexcept // симметричный 
+	void InorderPrint(std::ostream & str, shared_ptr<Node> thisNode) const noexcept // симметричный	
+	{
+		if (!thisNode)
+		{
+
+			return;
+		}
+
+		InorderPrint(str, thisNode->left_);
+		str << thisNode->value_ << " ";
+		InorderPrint(str, thisNode->right_);
+	}
+
+	auto size() const noexcept->size_t
+	{
+
+		return size_;
+	}
+
+
+
+	auto insert(const T & value) noexcept -> bool
+	{
+
+		shared_ptr<Node> thisNode = root_;
+		shared_ptr<Node> myNode = nullptr;
+
+		if (root_ == nullptr)
+		{
+			root_ = make_shared <Node>(value);
+			size_++;
+			return true;
+		}
+		while (thisNode)
+		{
+			myNode = thisNode;
+			if (value == myNode->value_)
+			{
+				return false;
+			}
+			else if (value < myNode->value_)
+			{
+				thisNode = myNode->left_;
+			}
+			else if (value > myNode->value_)
+			{
+				thisNode = myNode->right_;
+			}
+		}
+		if (value < myNode->value_)
+		{
+			myNode->left_ = make_shared <Node>(value);
+		}
+		else
+		{
+			myNode->right_ = make_shared <Node>(value);
+		};
+		size_++;
+		return true;
+	}
+
+
+	auto find(const T & value) const noexcept -> const T*
+	{
                auto thisNode = root_;
 
-    if (size_==0)
+	if (size_==0)
 
-    {
-        return nullptr;
-    }
+	{
+		return nullptr;
+	}
 
-    while (thisNode)
-    {
-        if (value < thisNode->value_)
-        {
-            thisNode = thisNode->left_;
-        }
-        else if (value > thisNode->value_)
-        {
-            thisNode = thisNode->right_;
-        }
-        else if (value == thisNode->value_)
-        {
-            return &thisNode->value_;
-        }
-    }
+	while (thisNode)
+	{
+		if (value < thisNode->value_)
+		{
+			thisNode = thisNode->left_;
+		}
+		else if (value > thisNode->value_)
+		{
+			thisNode = thisNode->right_;
+		}
+		else if (value == thisNode->value_)
+		{
+			return &thisNode->value_;
+		}
+	}
 
-    return nullptr;
-    }
-
-
-        auto comparison(const shared_ptr<Node> firstnode_, const shared_ptr<Node> secondnode_) const -> bool
-    {
-
-        if (firstnode_ == nullptr && secondnode_ == nullptr)
-        {
-            return(true);
-
-        }
-
-        else if (firstnode_ != nullptr && secondnode_ != nullptr)
-        {
-            return(
-                comparison(firstnode_->left_, secondnode_->left_) &&
-                comparison(firstnode_->right_, secondnode_->right_)
-                );
-        }
-        else return(false);
-    }
+	return nullptr;
+	}
 
 
+		auto comparison(const shared_ptr<Node> firstnode_, const shared_ptr<Node> secondnode_) const -> bool
+	{
 
-    auto remove(const T value_, shared_ptr<Node> node_) noexcept-> bool   // удаление узла,в котором находится объект со значением value
-    {
+		if (firstnode_ == nullptr && secondnode_ == nullptr)
+		{
+			return(true);
 
-        if (!node_)
-            return false;
+		}
 
-        else if (value_ < node_->value_)
-            node_->left_ = remove(node_->left_, value_);
+		else if (firstnode_ != nullptr && secondnode_ != nullptr)
+		{
+			return(
+				(firstnode_->value_ == secondnode_->value_) &&
+				comparison(firstnode_->left_, secondnode_->left_) &&
+				comparison(firstnode_->right_, secondnode_->right_)
+				);
+		}
+		else return(false);
+	}
 
 
-        else if (value_ > node_->value_)
-            node_->right_ = remove(node_->right_, value_);
 
-        else
-        {
-            if (node_->left_ = nullptr && node_->right_ == nullptr) // при сутствии узлов-потомков
-                shared_ptr<Node> node_ = nullptr;
+	auto remove(const T value_, shared_ptr<Node> node_) noexcept-> bool   // удаление узла,в котором находится объект со значением value
+	{
 
-            else if (node_->left_ == nullptr) // при наличии одного узла-потомка
-            {
-                shared_ptr<Node> tmp_ = node_;
-                node_ = node_->left_;
-            }
+		if (!node_)
+			return false;
 
-            else if (node_->right_ == nullptr)
-            {
-                shared_ptr<Node> tmp_ = node_;
-                node_ = node_->right_;
-                delete tmp_;
-            }
-            else // если их все же двое
-            {
-                shared_ptr<Node> chld_;
-                chld_ = node_->right_;
-                if ((chld_->left_ == nullptr) && (chld_->right_ == nullptr))
-                {
-                    node_ = chld_;
-                    delete chld_;
-                    node_->right_ = nullptr;
-                }
-                else //если правый потомок имеет потомков
-                {
-                    if ((node_->right_)->left_ != nullptr)
-                    {
-                        shared_ptr<Node> lnode_;
-                        shared_ptr<Node> lnodep_;
-                        lnodep_ = node_->right_;
-                        lnode_ = (node_->right_)->left_;
-                        while (lnode_->left != nullptr)
-                        {
-                            lnodep_ = lnode_;
-                            lnode_ = lnode_->left_;
+		else if (value_ < node_->value_)
+			node_->left_ = remove(node_->left_, value_);
 
-                        }
-                        node_->value_ = lnode_->value_;
-                        lnode_ = nullptr;
-                        lnodep_->left_ = nullptr;
-                    }
-                    else
-                    {
-                        shared_ptr<Node> tmp_;
-                        tmp_ = node_->right;
-                        node_->right_ = tmp_->right_;
-                        tmp_ = nullptr;
-                    }
-                }
-                return true;
-            }
-        }
-    }
+
+		else if (value_ > node_->value_)
+			node_->right_ = remove(node_->right_, value_);
+
+		else
+		{
+			if (node_->left_ = nullptr && node_->right_ == nullptr) // при сутствии узлов-потомков
+				shared_ptr<Node> node_ = nullptr;
+
+			else if (node_->left_ == nullptr) // при наличии одного узла-потомка
+			{
+				shared_ptr<Node> tmp_ = node_;
+				node_ = node_->left_;
+			}
+
+			else if (node_->right_ == nullptr)
+			{
+				shared_ptr<Node> tmp_ = node_;
+				node_ = node_->right_;
+				delete tmp_;
+			}
+			else // если их все же двое
+			{
+				shared_ptr<Node> chld_;
+				chld_ = node_->right_;
+				if ((chld_->left_ == nullptr) && (chld_->right_ == nullptr))
+				{
+					node_ = chld_;
+					delete chld_;
+					node_->right_ = nullptr;
+				}
+				else //если правый потомок имеет потомков
+				{
+					if ((node_->right_)->left_ != nullptr)
+					{
+						shared_ptr<Node> lnode_;
+						shared_ptr<Node> lnodep_;
+						lnodep_ = node_->right_;
+						lnode_ = (node_->right_)->left_;
+						while (lnode_->left != nullptr)
+						{
+							lnodep_ = lnode_;
+							lnode_ = lnode_->left_;
+
+						}
+						node_->value_ = lnode_->value_;
+						lnode_ = nullptr;
+						lnodep_->left_ = nullptr;
+					}
+					else
+					{
+						shared_ptr<Node> tmp_;
+						tmp_ = node_->right;
+						node_->right_ = tmp_->right_;
+						tmp_ = nullptr;
+					}
+				}
+				return true;
+			}
+		}
+	}
 
 
         static auto copy(shared_ptr<Node> tree) -> shared_ptr<Node>
-    {
-        if (!tree)
-        {
-            return NULL;
-        }
+	{
+		if (!tree)
+		{
+			return NULL;
+		}
+
+		
+		shared_ptr<Node> newnode = make_shared<Node>(tree->value);
+		newnode->left_ = copy(tree->left_);
+		newnode->right_ = copy(tree->right_);
+
+		return newnode;
+	}
 
 
-        shared_ptr<Node> newnode = make_shared<Node>(tree->value);
-        newnode->left_ = copy(tree->left_);
-        newnode->right_ = copy(tree->right_);
+	auto operator = (const BinarySearchTree& tree)->BinarySearchTree& //копирования
+	{
+		if (this == &tree)
+			return *this;
 
-        return newnode;
-    }
+		size_ = tree.size_;
+		root_ = root_->copy(tree.root_);
+		return *this;
 
-
-    auto operator = (const BinarySearchTree& tree)->BinarySearchTree& //копирования
-    {
-        if (this == &tree)
-            return *this;
-
-        size_ = tree.size_;
-        root_ = root_->copy(tree.root_);
-        return *this;
-
-    }
-
-
-
-    auto operator = (BinarySearchTree&& tree)->BinarySearchTree& //перемещения
-    {
-        if (this == &tree)
-            return *this;
-
-        size_ = tree.size_;
-        tree.size_ = 0;
-
-        delete root_;
-        root_ = tree.root_;
-        tree.root_ = nullptr;
-        return *this;
-    }
+	}
 
 
 
-    auto operator == (const BinarySearchTree& tree) const -> bool // сравнение
-    {
-        if (size_ != tree.size_)
-        {
-            return false;
-        }
+	auto operator = (BinarySearchTree&& tree)->BinarySearchTree& //перемещения
+	{
+		if (this == &tree)
+			return *this;
 
-        else
-        {
-           return comparison(root_, tree.root_);
-        }
-    }
+		size_ = tree.size_;
+		tree.size_ = 0;
+
+		delete root_;
+		root_ = tree.root_;
+		tree.root_ = nullptr;
+		return *this;
+	}
 
 
-    ~BinarySearchTree()
-    {
-        shared_ptr<Node> root_ = nullptr;
-    }
+
+	auto operator == (const BinarySearchTree& tree) const -> bool // сравнение
+	{
+		if (size_ != tree.size_)
+		{
+			return false;
+		}
+
+		else
+		{
+			return comparison(root_, tree.root_);
+		}
+	}
+
+
+	~BinarySearchTree()
+	{
+		shared_ptr<Node> root_ = nullptr;
+	}
 
 };
